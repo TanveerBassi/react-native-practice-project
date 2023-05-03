f86008fabe1f52b8f0947d4bda82829912d8e0bd
Tanveer Bassi <tanveer.bassi@bbc.co.uk>
2023-05-03 22:46:46 +0100

    network request made

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+import PalettePreview from '../components/PalettePreview';
+
+const URL = 'https://color-palette-api.kadikraman.now.sh/palettes';
 
 const Home = ({ navigation }) => {
+  const [palettes, setPalettes] = useState([]);
+
+  const handleFetchPalettes = useCallback(async () => {
+    const response = await fetch(URL);
+    if (response.ok) {
+      const palettes = await response.json();
+      setPalettes(palettes);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleFetchPalettes();
+  }, []);
+
   return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.push('ColorPalette')}>
-        <Text>Solarized</Text>
-      </TouchableOpacity>
-    </View>
+    <FlatList
+      style={styles.list}
+      data={palettes}
+      keyExtractor={item => item.paletteName}
+      renderItem={({ item }) => (
+        <PalettePreview
+          onPress={() => navigation.push('ColorPalette', item)}
+          palette={item}
+        />
+      )}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+});
 
 export default Home;
