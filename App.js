6b16dfc9424bbdcfd2d633bcb074fb47b36e47c7
Tanveer Bassi <tanveer.bassi@bbc.co.uk>
2023-05-03 21:19:34 +0100

    styling exercise

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Here are some boxes of different colours
+        </Text>
+        <View style={styles.cyanBox}>
+          <Text style={styles.text}>Cyan #2aa198</Text>
+        </View>
+        <View style={styles.blueBox}>
+          <Text style={styles.text}>Blue #268bd2</Text>
+        </View>
+        <View style={styles.magentaBox}>
+          <Text style={styles.text}>Magenta #FF00FF</Text>
+        </View>
+        <View style={styles.orangeBox}>
+          <Text style={styles.text}>Orange #FFA500</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  cyanBox: {
+    padding: 10,
+    borderRadius: 3,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#2aa198',
+  },
+  blueBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#268bd2',
+  },
+  magentaBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#FF00FF',
+  },
+  orangeBox: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#FFA500',
   },
 });
+
+export default App;
